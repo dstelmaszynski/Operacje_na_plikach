f8457bea8fead77dd7573a978b1a4265782de14f
Dominik <stelmaszynski.dominik@gmail.com>
2020-06-29 18:58:53 +0200

    ex1 done. Saving and editing file

--- src/main/java/Zad_1/Main.java
+++ src/main/java/Zad_1/Main.java
@@ -1,7 +1,35 @@
 package Zad_1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("plik.txt");
+
+        //sprawdzenie czy plik istnieje
+        boolean exists = file.exists();
+        System.out.println("This file exists: " + exists);
+
+        //sciezka absolutna
+        File absolutePath = file.getAbsoluteFile();
+        System.out.println("Ten plik jest w: " + absolutePath);
+
+        //zapis do pliku
+        File file1 = new File("wynik.txt");
+        PrintWriter zapis = new PrintWriter("wynik.txt");
+        zapis.println("Dominik jest fajny");
+        zapis.println("Beata jest fajniejsza");
+        zapis.println("Tosia jest najfajniejsza");
+        zapis.close();
+
+        //wyswietlanie zawartosci pliku
+        Scanner scanner = new Scanner(file1);
+        //petla while do wyswietlania do zdefiniowanego momentu .hasnext()
+        while (scanner.hasNext()){
+            String textReader = scanner.nextLine();
+            System.out.println(textReader);
+        }
     }
 }
