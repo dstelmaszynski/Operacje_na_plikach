5afb3cf81f3f13c45dfa9182ea7608511931c8ca
Dominik <stelmaszynski.dominik@gmail.com>
2020-06-30 17:38:52 +0200

    Ex. 2 - uzytkownik wprowadza tekst, zapis do pliku i odczyt

--- src/main/java/Zad_2/Main.java
+++ src/main/java/Zad_2/Main.java
@@ -1,7 +1,26 @@
 package Zad_2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //scanner do wprowadzanie tekstu przez uzytkowanika
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź tekst: ");
+        String textInput = scanner.nextLine();
+
+        //nowy plik + zapis
+        File file2 = new File("wynik_2.txt");
+        PrintWriter saveText = new PrintWriter("wynik_2.txt");
+        saveText.write(textInput);
+        saveText.close();
+
+        //wyswietlanie linii z pliku
+        Scanner scanner1 = new Scanner(file2);
+        System.out.println(scanner1.nextLine());
     }
 }
